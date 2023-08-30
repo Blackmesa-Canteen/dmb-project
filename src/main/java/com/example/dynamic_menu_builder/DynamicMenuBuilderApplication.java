98190f95f29b33e5f9226c5359304880c221874a
996Worker <shaotienlee@foxmail.com>
2023-08-30 10:08:48 +1000

    feat: init

---
+++ src/main/java/com/example/dynamic_menu_builder/DynamicMenuBuilderApplication.java
@@ -0,0 +1,13 @@
+package com.example.dynamic_menu_builder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DynamicMenuBuilderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DynamicMenuBuilderApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/dynamic_menu_builder/DynamicMenuBuilderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dynamic_menu_builder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DynamicMenuBuilderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
